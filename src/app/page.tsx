9fcde07add4ab6b2cc7b1b65eb01d3a5aad30bf2
brunoosuna8 <brunoosuna8@gmail.com>
2024-04-16 11:42:29 -0300

    "commit"

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main>
+          <h1>Hello</h1>
+          <a href="/api/auth/login">Login</a>
+      </main>
+  );
+}
