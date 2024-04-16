89e510f18732bf8aee05272e4258b14d56a37e95
brunoosuna8 <brunoosuna8@gmail.com>
2024-04-16 11:59:10 -0300

    sd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client"
 import Image from "next/image";
-
+import {useUser} from "@auth0/nextjs-auth0/client";
 export default function Home() {
+    const { user, error, isLoading } = useUser();
+
+    if(user) console.log(user)
   return (
       <main>
           <h1>Hello</h1>
-          <a href="/api/auth/login">Login</a>
+          {user ? <div
+              >
+              <h2>{user.nickname}</h2>
+                  <a href="/api/auth/logout">logout</a>
+              </div>
+              :
+              <a href="/api/auth/login">Login</a>
+          }
+
       </main>
   );
 }
